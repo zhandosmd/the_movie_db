2c7e8a37bf04cc40370ffb1b0b60b5f29ac03c4a
Zhandos Medetov <zhandosmedetov@gmail.com>
2021-12-29 17:17:12 +0600

    l32 - BNB

--- lib/ui/theme/app_colors.dart
+++ lib/ui/theme/app_colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
+abstract class AppColors{
   static const Color mainDarkBlue = Color.fromRGBO(3, 37, 65, 1);
 }
--- lib/ui/widgets/main_screen/main_screen_widget.dart
+++ lib/ui/widgets/main_screen/main_screen_widget.dart
@@ -8,14 +8,53 @@ class MainScreenWidget extends StatefulWidget {
 }
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
+  int _selectedTab = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Новости',
+    ),
+    Text(
+      'Фильмы',
+    ),
+    Text(
+      'Сериалы',
+    ),
+  ];
+
+  void onSelectTab(int index){
+    if(_selectedTab==index) return;
+
+    _selectedTab = index;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('TMDB'),
       ),
-      body: Container(),
+      body: Center(
+        child: _widgetOptions[_selectedTab],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: onSelectTab,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Новости'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_filter),
+            label: 'Фильмы'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            label: 'Сериалы'
+          ),
+        ],
+      ),
     );
   }
 }
-
